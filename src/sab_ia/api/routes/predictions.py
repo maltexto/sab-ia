7c3668607bc3b175b2af3aae2ba402088ab273ed
Joás <joas.junior@zup.com.br>
2025-05-17 17:19:10 -0300

    style: apply black code formatter

--- src/sab_ia/api/routes/predictions.py
+++ src/sab_ia/api/routes/predictions.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException, Query
 
+from sab_ia.services import process_audio_file
 
-predictions_router = APIRouter()
+predictions_router = APIRouter(prefix="/rest/v1")
+
 
 @predictions_router.post("/predict")
 async def predict_species(
-        audio_file: UploadFile = File(...),
-        min_confidence: float = Query(0.1, ge=0.0, lt=1.0, description="Minimum confidence threshold for predictions"),
-):
-    ...
+    audio_file: UploadFile = File(...),
+    min_confidence: float = Query(
+        0.1, ge=0.0, lt=1.0, description="Minimum confidence threshold for predictions"
+    ),
+): return process_audio_file(audio_file, min_confidence)
--- src/sab_ia/main.py
+++ src/sab_ia/main.py
@@ -4,10 +4,13 @@ from sab_ia.api.routes import predictions_router as prediction
 app = FastAPI(title="SAB_IA")
 app.include_router(prediction)
 
+
 @app.get("/")
 async def root():
     return {"status": "piu"}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
